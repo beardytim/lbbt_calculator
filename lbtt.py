6fd70f3c2a274e62b491cd468534000d09060e9f
Tim Simmonds <tim.simmonds1@gmail.com>
2022-04-09 17:52:02 +0100

    second commit

--- lbtt.py
+++ lbtt.py
@@ -1,19 +1,23 @@
+# Model to hold the tax data
 class TaxBand:
       def __init__(self, low, high, rate):
           self.low = low
           self.high = high
           self.rate = rate
-          
-def calculate_lbtt(property_value):
-    
+
+# Function that calculates the LBBT cost
+# Must be given a number (int or double) and returns a number
+def calculate_lbtt(property_value):  
     tax_bands = [TaxBand(0,145000,0),TaxBand(145000,250000,0.02),TaxBand(250000,325000,0.05),TaxBand(325000,750000,0.1),TaxBand(750000,float('inf'),0.12)]
     bands = []
     tax = 0
     
+    # Loop that removes tax bands from calculation that aren't needed
     for band in tax_bands:
         if band.low < property_value:
             bands.append(band)
     
+    # Loop that goes through the tax bands and calculates the tax
     for band in bands:
         if band.high < property_value:
             tax += (band.high-band.low)*band.rate
@@ -22,42 +26,31 @@ def calculate_lbtt(property_value):
     
     return round(tax,2)
 
-def validateInput(property_value):
-    print('okay')
-    # check if a int or double
-    # cheeck if a string
-    # check if negative
-    return True
-
-
+# Basic user input with validation
+def user_input():
+    print('Please enter the cost of the house without any grammer.')
+    while True:
+        try:
+            property_value = float(input("House Cost: "))
+            if(property_value<0):
+                print('You inputed a negative number.')
+            else:
+                return property_value
+        except ValueError:
+            print('You inputed a string.')
 
 if __name__ == "__main__":
-    validated = False
-    while not validated:
-        property_value = input("House Cost: ")
-        validated = validateInput(property_value)
-        
-    # poperty_value = 325000
+    # User input with validation
+    property_value = user_input()
+    
+    # User input without validaiton
+    # property_value = float(input("House Cost: "))
+    
+    # Hard coded value
+    # property_value = 325000
+    
     tax = calculate_lbtt(property_value)
+    
+    # Print the return on its own or in a sentance
     print(tax)
-
-
-# def validateInput():
-#     print('Please enter the cost of the house without any grammer.')
-#     while True:
-#         try:
-#             property_value = float(input("House Cost: "))
-#             if(property_value<0):
-#                 print('You inputed a negative number.')
-#             else:
-#                 return property_value
-#         except ValueError:
-#             print('You inputed a string.')
-
-# if __name__ == "__main__":
-#     property_value = validateInput()
-#     # poperty_value = 325000
-#     tax = calculate_lbtt(property_value)
-#     print(tax)
-
-
+    # print("The LBTT cost is £{:.2f}".format(tax))
--- lbtt_test.py
+++ lbtt_test.py
@@ -3,15 +3,27 @@ from lbtt import calculate_lbtt
 
 class TestLBTT(unittest.TestCase):
     def test_no(self):
-        self.assertEqual(calculate_lbtt(100000),0)
+        self.assertEqual(calculate_lbtt(100000),0.0)
     def test_low(self):
-        self.assertEqual(calculate_lbtt(200000),1100)
+        self.assertEqual(calculate_lbtt(200000),1100.0)
     def test_mid(self):
-        self.assertEqual(calculate_lbtt(300000),4600)
+        self.assertEqual(calculate_lbtt(300000),4600.0)
     def test_high(self):
-        self.assertEqual(calculate_lbtt(400000),13350)
+        self.assertEqual(calculate_lbtt(400000),13350.0)
     def test_top(self):
-        self.assertEqual(calculate_lbtt(800000),54350)
+        self.assertEqual(calculate_lbtt(800000),54350.0)
+    def test_edge_one(self):
+        self.assertEqual(calculate_lbtt(145001),0.02)
+    def test_edge_two(self):
+        self.assertEqual(calculate_lbtt(249999),2099.98)
+    def test_edge_three(self):
+        self.assertEqual(calculate_lbtt(250000),2100.0)
+    def test_edge_four(self):
+        self.assertEqual(calculate_lbtt(250001),2100.05)
+    def test_edge_five(self):
+        self.assertEqual(calculate_lbtt(750000),48350.0)
+    def test_edge_six(self):
+        self.assertEqual(calculate_lbtt(2000000),198350.0)
 
 if __name__ == '__main__':
     unittest.main()

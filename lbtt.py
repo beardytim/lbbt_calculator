e52af4748ec217261402d68be02104fe2e453be9
Tim Simmonds <tim.simmonds1@gmail.com>
2022-04-10 20:02:17 +0100

    small change

--- lbtt.py
+++ lbtt.py
@@ -1,13 +1,13 @@
-# Model to hold the tax data
 class TaxBand:
-      def __init__(self, low, high, rate):
-          self.low = low
-          self.high = high
-          self.rate = rate
+    '''A model to store a tax band.'''
+    def __init__(self, low, high, rate):
+        self.low = low
+        self.high = high
+        self.rate = rate
 
-# Function that calculates the LBBT cost
-# Must be given a number (int or double) and returns a number
+
 def calculate_lbtt(property_value):  
+    '''Function that calculates the LBBT cost. Must be given a number and returns a number.'''
     tax_bands = [TaxBand(0,145000,0),TaxBand(145000,250000,0.02),TaxBand(250000,325000,0.05),TaxBand(325000,750000,0.1),TaxBand(750000,float('inf'),0.12)]
     bands = []
     tax = 0
@@ -26,9 +26,8 @@ def calculate_lbtt(property_value):
     
     return round(tax,2)
 
-# Basic user input with validation
 def user_input():
-    print('Please enter the cost of the house without any grammer.')
+    '''Function that takes user input and validates it. Returns a number.'''
     while True:
         try:
             property_value = float(input("House Cost: "))
@@ -37,20 +36,17 @@ def user_input():
             else:
                 return property_value
         except ValueError:
-            print('You inputed a string.')
+            print('Please only input numbers.')
 
 if __name__ == "__main__":
     # User input with validation
     property_value = user_input()
     
-    # User input without validaiton
+    # User input without validation
     # property_value = float(input("House Cost: "))
     
     # Hard coded value
     # property_value = 325000
     
     tax = calculate_lbtt(property_value)
-    
-    # Print the return on its own or in a sentance
     print(tax)
-    # print("The LBTT cost is £{:.2f}".format(tax))
